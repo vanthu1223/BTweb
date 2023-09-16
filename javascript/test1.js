94e993a4751807fb6246c235e37baa498eb72cc0
thu <thu.vo25@student.passerellesnumeriques.org>
2023-09-16 09:41:20 +0700

    Reponsive

---
+++ javascript/test1.js
@@ -0,0 +1,13 @@
+        
+function kq(){
+    var a= "Hello";
+    var b="World ??";
+    var c=a+" "+b;
+    document.getElementById('kq').innerHTML=c;
+    alert(c)
+    document.write(c);
+}
+   
+      
+        
+        
---
+++ light/diem/diem.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    var a=document.getElementById('input-1').value;
+    a=parseInt(a);
+    var b=document.getElementById('input-2').value;
+    b=parseInt(b);
+    var se=document.getElementById('select');
+    var value=se.options[se.selectedIndex].value;
+    value=parseInt(value);
+    var kq;
+    switch(value){
+        case 1:
+            
+
+    }
+
+}
